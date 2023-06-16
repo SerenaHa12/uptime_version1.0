c4ffbb35f94d7e7fa047e3f5fe4826f4dce148f0
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-16 23:48:53 +0700

    fix login.jsx

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,50 +1,38 @@
-import './login.css';
-import item_1 from '../assets/img/logo-dark.svg'
+import "./login.css";
+import logo_dark from "../assets/img/logo-dark.svg";
+import item_0 from "../assets/img/item-0.png";
 export const Login = () => {
-    return (
-        <div className='login'>
-            <div className='login-intro'>
-                <div className='login-logo'></div>
-                    <img  src={item_1} />
-                    <h1>SMART EASM</h1>
-                    <p>
-                        SMART EASM - a comprehensive solution to proactively protect your online presence. 
-                        Identify vulnerabilities, receive actionable insights, and ensure continuous monitoring 
-                        for enhanced security. Safeguard your critical assets with confidence.
-                    </p>
-                    <h2 className='logo-title'>Threat Intelligence</h2>
-                     <div className='logo-tool'>
-                        <h2>Uptime Monitoring</h2>
-                        <p>
-                        Uptime monitoring tools track and monitor online resources, minimizing downtime and 
-                        maximizing availability for optimal user experience. They ensure business continuity 
-                        by promptly alerting to issues and enabling proactive problem resolution.
-                        </p>
-                        <button>Try Demo</button>
-                    </div>
-                    <h2>Fraud Protection</h2>
-                    <h2>Digital Risk Protection</h2>
-                </div>
-            <div className='login-form'>
-                <form>
-                    <h1>SIGN IN</h1>
-                    <p>Please sign in using your organization SSO credentials</p>
-                    <div className='login-email'>
-                    <input type="email" placeholder='Email'></input>
-                    <p>Enter your email organization</p>
-                    </div>
-                    <div className='login-password'>
-                    <input type="password" placeholder='Password'></input>
-                    <p>Enter your password</p>
-                    </div>
-                    <p><a href="#">Forgot your password?</a></p>
-                    <button>Sign in</button>
-                </form>
-                <div className='login-lisence'>
-                    <p>+84 373506620 . <span><a href='#'>workspace@gmail.com</a></span></p>
-                    <p>@ 2022 - 2023 Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                </div>
+  return (
+    <div className="login">
+
+        <div className="login-intro">
+            <a href="#"><img src={logo_dark} alt="logo" /></a>
+            <h1>SMART EASM</h1>
+            <p>
+                SMART EASM - a comprehensive solution to proactively 
+                protect your online presence. Identify vulnerabilities, 
+                receive actionable insights, and ensure continuous 
+                monitoring for enhanced security. Safeguard your 
+                critical assets with confidence.
+            </p>
+            <div className="logo-tool">
+                <h2>Uptime Monitoring</h2>
+                <p>
+                    Uptime monitoring tools track and monitor online 
+                    resources, minimizing downtime and maximizing availability 
+                    for optimal user experience. They ensure business continuity 
+                    by promptly alerting to issues and enabling proactive 
+                    problem resolution.
+                </p>
+                <button>Try Demo</button>
             </div>
-      </div>
-    )
-}
+            <h2>Fraud Protection</h2>
+            <h2>Digital Risk Protection</h2>
+            <h2>Threat Intelligence</h2>
+            <div className="login-demo">
+                <img src={item_0} alt="product demo" />
+            </div>
+        </div>
+    </div>
+  );
+};
